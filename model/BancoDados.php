20c8ca630eee88ee185aeb06e35bc68cf1976dd1
Matheus Gonçalves <matheushtht@gmail.com>
2025-05-19 16:17:41 -0300

    Movendo BancoDeDados para model, e adicionando funcionalidade de conectar-se ao banco de dados postgresql

---
+++ model/BancoDados.php
@@ -0,0 +1,22 @@
+<?php
+class BancoDados {
+    private $host = "127.0.0.1";
+    private $port = "5432";
+    private $dbname = "tde3";
+    private $user = "postgres";
+    private $password = "88548582";
+    private $conexao;
+        
+    public function conectar() : bool {
+        $conexao = pg_connect("host={$this->host} port={$this->port} dbname={$this->dbname} user={$this->user} password={$this->password}");
+
+        if(!$conexao){
+            echo "Falha na conexao com o banco de dados" . preg_last_error();
+            return false;
+        }
+
+        $this->conexao = $conexao;
+        return true;
+    }
+}
+?>
